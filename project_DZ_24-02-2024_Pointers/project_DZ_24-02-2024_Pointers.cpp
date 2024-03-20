730280a556af6b7438765d8cdc4b46a66acf5870
SVARGus <kyznetsovpn@mail.ru>
2024-03-20 21:26:12 +0300

    ДЗ на указатели завершено 20.03.2024

--- project_DZ_24-02-2024_Pointers/project_DZ_24-02-2024_Pointers.cpp
+++ project_DZ_24-02-2024_Pointers/project_DZ_24-02-2024_Pointers.cpp
@@ -45,6 +45,9 @@ void swap(int* ptr1, int* ptr2); // 2, 6
 void ReplaceMirrorMussPTR(int* ptr, int size); // 2
 void MirrorM1_in_M2(int* ptr1, int* ptr2, int size); // 3
 void CheckSign(int* x); // 5
+double CulcPtr(double* ptr1, double* ptr2); // 7
+template<typename T>
+T SumMussptr(T* ptr, int size); // 8
 
 
 template<typename T>
@@ -110,10 +113,44 @@ void CheckSign(int* x) // Функция для определения знаа 
 		std::cout << "Число " << *x << " нулевое" << std::endl;
 }
 
+double CulcPtr(double* ptr1, double* ptr2) // Примитивный калбкулятор (Задание 7)
+{
+	char Ch{};
+	std::cout << "Укажите действие с числами (+, -, *, /): ";
+	std::cin >> Ch;
+	switch (Ch)
+	{
+	case '+':
+		return *ptr1 + *ptr2;
+	case '-':
+		return *ptr1 - *ptr2;
+	case '*':
+		return *ptr1 * *ptr2;
+	case '/':
+		return *ptr1 / *ptr2;
+	}
+}
+
+template<typename T>
+T SumMussptr(T* ptr, int size) // Суммирование массива (задание 8)
+{
+	T Summ{};
+	for (int i = 0; i < size; i++)
+	{
+		Summ += *(ptr + i);
+	}
+	// Второй вариант суммирования
+	/*for (int i = 0; i < size; i++, ptr++)
+	{
+		Summ += *ptr;
+	}*/
+	return Summ;
+}
+
 int main_1() // Задание 1. Для работы заменить main_1 на main
 {
-	setlocale(LC_ALL, "ru");
-	srand(time(NULL));
+	/*setlocale(LC_ALL, "ru");
+	srand(time(NULL));*/
 
 	const int SIZE{ 10 };
 	int Muss1[SIZE]{};
@@ -135,8 +172,8 @@ int main_1() // Задание 1. Для работы заменить main_1 н
 
 int main_2() // Задание 2. Для работы заменить main_2 на main
 {
-	setlocale(LC_ALL, "ru");
-	srand(time(NULL));
+	/*setlocale(LC_ALL, "ru");
+	srand(time(NULL));*/
 
 	const int SIZE{ 10 };
 	int Muss[SIZE]{};
@@ -152,8 +189,8 @@ int main_2() // Задание 2. Для работы заменить main_2 н
 
 int main_3() // Задание 3. Для работы заменить main_3 на main
 {
-	setlocale(LC_ALL, "ru");
-	srand(time(NULL));
+	/*setlocale(LC_ALL, "ru");
+	srand(time(NULL));*/
 
 	const int SIZE{ 10 };
 	int Muss1[SIZE]{};
@@ -174,7 +211,7 @@ int main_3() // Задание 3. Для работы заменить main_3 н
 
 int main_4() // Задание 4. Для работы заменить main_4 на main
 {
-	setlocale(LC_ALL, "ru");
+	//setlocale(LC_ALL, "ru");
 	//srand(time(NULL));
 
 	int Num1{};
@@ -192,7 +229,7 @@ int main_4() // Задание 4. Для работы заменить main_4 н
 
 int main_5() // Задание 5. Для работы заменить main_5 на main
 {
-	setlocale(LC_ALL, "ru");
+	//setlocale(LC_ALL, "ru");
 	//srand(time(NULL));
 
 	int Num{};
@@ -205,7 +242,7 @@ int main_5() // Задание 5. Для работы заменить main_5 н
 
 int main_6() // Задание 6. Для работы заменить main_6 на main
 {
-	setlocale(LC_ALL, "ru");
+	//setlocale(LC_ALL, "ru");
 	//srand(time(NULL));
 
 	int Num1{};
@@ -221,9 +258,96 @@ int main_6() // Задание 6. Для работы заменить main_6 н
 	return 0;
 }
 
-int main() // Задание 7. Для работы заменить main_7 на main
+int main_7() // Задание 7. Для работы заменить main_7 на main
+{
+	/*setlocale(LC_ALL, "ru");*/
+	
+	double Num1{};
+	double Num2{};
+	double* ptrN1{ nullptr };
+	double* ptrN2{ nullptr };
+	ptrN1 = &Num1;
+	ptrN2 = &Num2;
+	std::cout << "Произведем вычисление с числом ";
+	std::cin >> Num1;
+	std::cout << " и числом ";
+	std::cin >> Num2;
+	std::cout << "Результат вычисления " << CulcPtr(ptrN1, ptrN2) << std::endl;
+
+	return 0;
+}
+
+int main_8() // Задание 8. Для работы заменить main_8 на main
+{
+	/*setlocale(LC_ALL, "ru");
+	srand(time(NULL));*/
+
+	const int SIZE{ 10 };
+	int Muss[SIZE]{};
+	int* ptrM{ nullptr };
+	GenMuss(Muss, SIZE);
+	PrintMuss(Muss, SIZE);
+	ptrM = Muss;
+	std::cout << "Сумма элементов массива = " << SumMussptr(ptrM, SIZE) << std::endl;
+
+	return 0;
+}
+
+int main()
 {
 	setlocale(LC_ALL, "ru");
+	srand(time(NULL));
+
+	std::cout << "Меню по домашнему заданию на Указатели" << std::endl;
+	int Menu{};
+	do
+	{
+		std::cout << "Введите номер задания от 1 до 8, 0 - выход: ";
+		std::cin >> Menu;
+		switch (Menu)
+		{
+		case 1:
+			std::cout << "Задание 1:\nИспользуя два указателя на массив целых чисел, скопировать один массив в другой.\nИспользовать в программе арифметику указателей для продвижения по массиву, а также оператор разыменования." << std::endl << std::endl;
+			main_1();
+			std::cout << std::endl;
+			break;
+		case 2:
+			std::cout << "Задание 2:\nИспользуя указатель на массив целых чисел, изменить порядок следования элементов массива на противоположный.\nИспользовать в программе арифметику указателей для продвижения по массиву, а также оператор разыменования." << std::endl << std::endl;
+			main_2();
+			std::cout << std::endl;
+			break;
+		case 3:
+			std::cout << "Задание 3:\nИспользуя два указателя на массивы целых чисел, скопировать один массив в другой так,\nчтобы во втором массиве элементы находились в обратном порядке.\nИспользовать в программе а рифметику указателей для продвижения по массиву, а также оператор разыменования." << std::endl << std::endl;
+			main_3();
+			std::cout << std::endl;
+			break;
+		case 4:
+			std::cout << "Задание 4:\nИспользуя указатели и оператор разыменования, определить наибольшее из двух чисел." << std::endl << std::endl;
+			main_4();
+			std::cout << std::endl;
+			break;
+		case 5:
+			std::cout << "Задание 5:\nИспользуя указатели и оператор разыменования, определить знак числа, введённого с клавиатуры." << std::endl << std::endl;
+			main_5();
+			std::cout << std::endl;
+			break;
+		case 6:
+			std::cout << "Задание 6:\nИспользуя указатели и оператор разыменования, обменять местами значения двух переменных." << std::endl << std::endl;
+			main_6();
+			std::cout << std::endl;
+			break;
+		case 7:
+			std::cout << "Задание 7:\nНаписать примитивный калькулятор, пользуясь только указателями." << std::endl << std::endl;
+			main_7();
+			std::cout << std::endl;
+			break;
+		case 8:
+			std::cout << "Задание 8:\nИспользуя указатель на массив целых чисел, посчитать сумму элементов массива.\nИспользовать в программе арифметику указателей для продвижения по массиву, а также оператор разыменования." << std::endl << std::endl;
+			main_8();
+			std::cout << std::endl;
+			break;
+		}
+	} while (Menu != 0);
 
 	return 0;
 }
