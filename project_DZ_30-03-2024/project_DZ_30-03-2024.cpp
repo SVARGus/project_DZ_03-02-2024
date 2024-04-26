9cc682c36ec7722e8eeee7bdc0e93cf8548b927d
SVARGus <kyznetsovpn@mail.ru>
2024-04-26 17:35:02 +0300

    В процессе

--- project_DZ_30-03-2024/project_DZ_30-03-2024.cpp
+++ project_DZ_30-03-2024/project_DZ_30-03-2024.cpp
@@ -37,12 +37,45 @@ int main_5()
     return 0;
 }
 
-int main()
+void Print(char ABC[], int SIZE)
+{
+    for (int i = 0; i < SIZE; i++)
+        std::cout << ABC[i] << " ";
+    std::cout << std::endl;
+}
+
+int main_6()
 {
     setlocale(LC_ALL, "ru");
+    srand(time(NULL));
 
     std::cout << "Задание #6\nДома у Ксюши было 2 одинаковых набора кубиков из английских букв, но во время очередной уборки один из кубиков затерялся.\nПомогите Ксюше определить, какой из кубиков отсутствует в одном из наборов." << std::endl;
-
+    const int SIZE{ 26 };
+    char ABC[SIZE]{ 'a' };
+    char ABCloss[SIZE-1]{};
+    int loss{};
+    char Loss_abc{};
+    loss = rand() % (SIZE + 1); // Рандомно определяет какой кубик потерян
+    for (int i = 1; i < SIZE; i++) // Заполняем набор кубиками с английскими буквами попорядку
+        ABC[i] = ABC[i - 1] + 1;
+    for (int i = 0, k = 0; i < SIZE; i++, k++) // Заполняем второй набор кубиками, но один кубик рандомно потерян
+    {
+        if (i == loss)
+            ++i;
+        ABCloss[k] = ABC[i];
+    }
+    std::cout << "Выведем кубики из полного набора: ";
+    Print(ABC, SIZE);
+    std::cout << "Также выведем кубики из набора с одним потеренным кубиком: ";
+    Print(ABCloss, SIZE-1);
+    for (int i = 0; i < SIZE; i++)
+    {
+        if (i < SIZE - 1)
+            Loss_abc ^= ABCloss[i] ^ ABC[i];
+        else
+            Loss_abc ^= ABC[i];
+    }
+    std::cout << "Потерен кубик: " << Loss_abc << std::endl;
 
     return 0;
 }
