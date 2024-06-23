6b74b488aa2e75e6445e2cd0c588a594f3b9d992
SVARGus <kyznetsovpn@mail.ru>
2024-06-23 23:57:49 +0300

    Написание MyString

--- MyString_vork/MyString_vork/MyString_vork.cpp
+++ MyString_vork/MyString_vork/MyString_vork.cpp
@@ -4,11 +4,22 @@
 #include <iostream>
 #include "mystring.h"
 
+int mystring::count = 0;
+
 int main()
 {
     setlocale(LC_ALL, "ru");
-
-    std::cout << "Hello World!\n";
+    char xy[] = "help";
+    std::cout << xy << " " << strlen(xy) << std::endl;
+    mystring name{xy};
+    name.print(); // проверка метода печати
+    mystring name1{ name };
+    name1.print();
+    char xy1[] = "forever";
+    name.setmString(xy1);
+    name.print();
+    mystring Y = name + name1;
+    Y.print();
 
     return 0;
 }
--- MyString_vork/MyString_vork/mystring.cpp
+++ MyString_vork/MyString_vork/mystring.cpp
@@ -1,3 +1,60 @@
 ﻿#include "mystring.h"
 #include <iostream>
 
+mystring::mystring(){}
+
+mystring::mystring(char* name) // ОК
+{
+	size = strlen(name);
+	mString = new char[size + 1];
+	for (int i = 0; i <= size; i++)
+		mString[i] = name[i];
+	++count;
+}
+
+mystring::mystring(const mystring& name) : size{ name.size }, mString{ new char[size + 1] {} } // OK
+{
+	for (int i = 0; i <= size; ++i)
+		this->mString[i] = name.mString[i];
+	++count;
+}
+
+mystring::~mystring() // ОК
+{
+	delete[] mString;
+	--count;
+	std::cout << "Строка удалена, счетчик = " << count << std::endl;
+}
+
+void mystring::setmString(char* name) // OK
+{
+	size = strlen(name);
+	mString = new char[size + 1];
+	for (int i = 0; i <= size; ++i)
+		mString[i] = name[i];
+}
+
+mystring operator+ (const mystring& name1, const mystring& name2)
+{
+	mystring name3;
+	name3.size = name1.size + name2.size;
+	name3.mString = new char[name3.size + 1];
+	for (int i = 0, j = 0; i <= name3.size; ++i)
+	{
+		if (i < name1.size)
+			name3.mString[i] = name1.mString[i];
+		else
+		{
+			name3.mString[i] = name2.mString[j];
+			++j;
+		}
+	}
+	mystring::setStaticCount();
+	return name3;
+}
+
+void mystring::print() const // ОК
+{
+	std::cout << mString << " " << size << " " << strlen(mString) << " Счетчик = " << count << std::endl;
+
+}
