26be671f3ce129abcde995b3c33507450c6365dc
SVARGus <kyznetsovpn@mail.ru>
2024-07-03 23:40:01 +0300

    Создание шаблона классов однонаправленного списка - непонятная ошибка

---
+++ unidirectional_list/unidirectional_list.cpp
@@ -0,0 +1,69 @@
+﻿/*
+Домашнее задание от 02.07.2024
+
+Переделать класс однонаправленный список для переменной одного типа в шаблон и тест
+
+*/
+#include <iostream>
+
+template<class T>
+class Node {
+    friend class OneLinkedList<T>;
+    T content;
+    Node* next;
+
+public:
+    explicit Node(T content) : content{content}, next{nullptr}{}
+    void setContent(T content) {
+        this->content = content;
+    }
+    T getContent() const {
+        return content;
+    }
+    void printContent() {
+        std::cout << "Content = " << content << std::endl;
+    }
+};
+
+template<class T>
+class OneLinkedList {
+    Node<T>* head;
+    Node<T>* tail;
+    
+public:
+    OneLinkedList() : head{nullptr}, tail{nullptr}{}
+
+    ~OneLinkedList() {
+        // дописать как разберусь с созданием, добавлением и перейду к удалению
+    }
+
+    void pushFront(T content) {
+        Node<T> *node = new Node<T>(content);
+        node->next = head;
+        head = node;
+        if (!tail)
+            tail = node;
+    }
+    void pushBack(T content) {
+
+    }
+
+    void popFront() {
+
+    }
+    void popBack() {
+
+    }
+
+
+};
+
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+
+
+    return 0;
+}
