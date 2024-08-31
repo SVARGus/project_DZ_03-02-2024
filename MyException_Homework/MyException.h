66b329a4589263f6c2f13c3709a5894dec3385d1
SVARGus <kyznetsovpn@mail.ru>
2024-08-31 18:19:20 +0300

    Создан класс MyExeption, после завершения Домашнего задания по Шаблону класса Матрицы добавить в задание и сделать рефакторинг для поимки исключений

--- MyException_Homework/MyException.h
+++ MyException_Homework/MyException.h
@@ -5,8 +5,15 @@ using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
+using std::exception;
 
-class MyEsception
+class MyEsception : public exception
 {
+	int dateState;
+
+public:
+	MyEsception(const char* message, int dateState) :exception(message) { this->dateState = dateState; } // на случай если нужно передать код ошибки который будет подробно описан в документации на класс (библиотеку, программу...)
+	MyEsception(const char* message) :exception(message) {}
+	int getDateState() { return dateState; }
 };
 
--- MyException_Homework/MyException_Homework.cpp
+++ MyException_Homework/MyException_Homework.cpp
@@ -12,17 +12,36 @@
 
 #include <iostream>
 #include "MyException.h"
+// подключить класс матрица из ДЗ по шаблонам классов (также добавить туда поимку исключений)
 
 using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
 
+double deleteNum(int x, int y) {
+	if (y == 0)
+	{
+		throw MyEsception("Делить на 0 нельзя");
+	}
+	return (double)x / y;
+}
 
 int main()
 {
     setlocale(LC_ALL, "ru");
 
+	int x{ 5 };
+	int y{};
+	try
+	{
+		cout << deleteNum(x, y);
+	}
+	catch (MyEsception &ex)
+	{
+		cout << ex.what() << endl;
+		cout << "Код ошибки: " << ex.getDateState() << endl;
+	}
 
     return 0;
 }
