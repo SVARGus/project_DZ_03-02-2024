13015b668c4a571e60cd4a41b892843095978ae7
SVARGus <kyznetsovpn@mail.ru>
2024-08-31 13:35:41 +0300

    Начало работы над ДЗ, создание своего класса Exception

---
+++ MyException_Homework/MyException_Homework.cpp
@@ -0,0 +1,29 @@
+﻿/*
+Создать свой класс Exception
+
+Задание 2.
+
+1) Деление на ноль
+2) Ввод числа вместо буквы
+3) Выход за границы массива(матрицы). Матрица 2х2, мы, к примеру, перемножаем элементы (3;3)
+4) Неверный тип входных параметров(вместо Int пишем double)
+
+*/
+
+#include <iostream>
+#include "MyException.h"
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+
+    return 0;
+}
+
