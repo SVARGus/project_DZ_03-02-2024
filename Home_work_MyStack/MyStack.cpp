5ed33768a02bd791a2032ecd1a487059bc818918
SVARGus <kyznetsovpn@mail.ru>
2024-09-04 22:17:31 +0300

    Завершено задание на проверку скобок через Стэк, позже добавить на поимку исключения на переполнение Стэка

---
+++ Home_work_MyStack/MyStack.cpp
@@ -0,0 +1,30 @@
+﻿#include "MyStack.h"
+
+void MyStack::Push(char s) {
+	if (!IsFull())
+	{
+		str[++top] = s;
+	}
+	else
+	{
+		//вставить поимку исключения при переполнения стека
+	}
+}
+char MyStack::Pop() {
+	if (!IsEmpty())
+		return str[top--];
+	else
+		return 0;
+}
+void MyStack::Clear() {
+	top = empty;
+}
+bool MyStack::IsEmpty() {
+	return top == empty;
+}
+bool MyStack::IsFull() {
+	return top == full;
+}
+int MyStack::GetCount() {
+	return top + 1;
+}
---
+++ Home_work_MyStack/MyStack.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+class MyStack
+{
+	int empty{ -1 };
+	int full{ 20 };
+	int top{ empty };
+	char* str{};
+public:
+	MyStack() { str = new char[full + 1]; };
+	MyStack(int full) : full{ full } { str = new char[this->full + 1]; };
+	~MyStack() { delete[] str; str = nullptr; };
+	void Push(char s);
+	char Pop();
+	void Clear();
+	bool IsEmpty();
+	bool IsFull();
+	int GetCount();
+};
