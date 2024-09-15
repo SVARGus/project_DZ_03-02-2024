1305c2c385802f82e98f7022d37fc5d132183fb2
SVARGus <kyznetsovpn@mail.ru>
2024-09-15 22:06:24 +0300

    Начало проработки Задания: Автосалон с применением STL

---
+++ Car_Shop_STL_Work/Product.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+#include <iostream>
+
+using std::cin;
+using std::endl;
+using std::cout;
+using std::string;
+
+// Виртуальный класс Продукт для других подкласов как Машина, Мотоцикл, Грузовик и прочие
+
+class Product
+{
+public:
+	// Геттеры для вывода информации о транспортном средстве
+	virtual string getVehicleTupe() const = 0; // Тип транспортного средства: Автомобиль, мотоцикл, грузовик
+	virtual string getBrand() const = 0; // Бренд ТС
+	virtual string getModel() const = 0; // Модель ТС
+	virtual string getColor() const = 0; // Цвет ТС
+	virtual int getNumSeat() const = 0; // Количество мест в ТС
+	virtual int getEngineVolume() const = 0; // Объем двигателя ТС
+	virtual double getPrice() const = 0; // Вывод цены ТС
+	virtual int getCountProduct() const = 0; // Количество ТС в наличии
+
+	// Сеттеры для изменения переменных
+	virtual void setPrice(const double Price) = 0; // Для изменения цены товара (например в случае скидки или после покупки ТС для последующей перепродажи)
+
+	// Булевские операторы сравнения
+	virtual bool operator== (const Product& other) const = 0; // оператор перегружен для сортировки
+	virtual bool operator< (const Product& other) const = 0; // оператор перегружен для сортировки
+	virtual bool operator+ (const Product& other) = 0; // оператор сложения, будет по факту увеличивать количество ТС в автосалоне, но при условии если данное ТС уже есть в базе автосалона. Если ТС нету, то будет произведено добавление в базу
+
+	// Методы вывода
+	virtual void print() const = 0; // Вывод на печать
+
+	// Виртуальный деструктор
+	virtual ~Product() = default;
+};
+
---
+++ Car_Shop_STL_Work/Vehicle.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+#include <iostream>
+#include "Product.h" 
+
+using std::cin;
+using std::endl;
+using std::cout;
+using std::string;
+
+class Vehicle : public Product
+{
+	// Базовые характеристики
+	string VehicleTupe{};
+	string Brand{};
+	string Model{};
+	string Color{};
+	int NumSeat{};
+	int EngineVolume{};
+	double Price{};
+	int CountProduct{};
+	// Дополнительные характеристики
+	string BodyType{}; // Тип кухова // Подумать как сделать тип кузова через enum
+
+public:
+
+
+	string getVehicleTupe() const override { return VehicleTupe; }
+	string getBrand() const override { return Brand; }
+	string getModel() const override { return Model; }
+	string getColor() const override { return Color; }
+	int getNumSeat() const override { return NumSeat; }
+	int getEngineVolume() const override { return EngineVolume; }
+	double getPrice() const override { return Price; }
+	int getCountProduct() const override { return CountProduct; }
+	string getBodyType() const { return BodyType; }
+
+	void setPrice(const double Pr) override { this->Price = Price; }
+
+	bool operator== (const Product& other) const override; // Продумать как реализовать данный оператор в .срр файле
+	bool operator< (const Product& other) const override; // Продумать как реализовать данный оператор в .срр файле
+	bool operator+ (const Product& other) override; // Продумать как реализовать данный оператор в .срр файле // может отказаться от данного оператора в пользу метода сложения в классе автосалона??
+	void print() const override;
+};
+
